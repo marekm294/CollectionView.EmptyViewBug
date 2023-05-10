cabbbc5421b9f4f18a2ac35bc7b64844666a2a9a
Marek Mička <marek.micka@conware.eu>
2023-05-10 15:28:00 +0200

    Add project files.

---
+++ CollectionView.EmptyViewBug/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CollectionView.EmptyViewBug;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ CollectionView.EmptyViewBug/Pages/CollectionViewInVerticalStackEmptyViewTemplate.xaml.cs
@@ -0,0 +1,9 @@
+namespace CollectionView.EmptyViewBug.Pages;
+
+public partial class CollectionViewInVerticalStackEmptyViewTemplate : ContentPage
+{
+	public CollectionViewInVerticalStackEmptyViewTemplate()
+	{
+		InitializeComponent();
+	}
+}
